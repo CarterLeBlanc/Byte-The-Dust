cf31846da931234ee3266e6844293d98ee3f9289
PriceSarah <sfp1024@gmail.com>
2020-06-26 09:24:52 -0500

    Fixed the attack code (still doesnt worl) and added temporary walls

--- Assets/Scripts/EnemyAttackBehavior.cs
+++ Assets/Scripts/EnemyAttackBehavior.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class EnemyAttackBehavior : MonoBehaviour
 {
 
-    public float attack = 10.0f;
-    public float maxHealth = 20.0f;
-    public float currentHealth;
+    public Transform enemyAttackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask playerLayer;
+
+
+    private float attack = EnemyBaseBehavior.GetEnemyDamage();
+    private float maxHealth = EnemyBaseBehavior.GetEnemyMaxHealth();
+    private float currentHealth = EnemyBaseBehavior.GetEnemyCurrentHealth();
+
+    private float attackRate = EnemyBaseBehavior.GetEnemyAttackRate();
+    float nextAttackTime = 0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+
+            //NOTICE: ATTACK DOES DOUBLE DAMAGE! NEEDS TO BE FIXED
+            Attack();
+            nextAttackTime = Time.time + 1f / attackRate;
+
+        }
+    }
+
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
 
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             Die();
         }
@@ -33,9 +57,28 @@ public class EnemyAttackBehavior : MonoBehaviour
         this.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Attack()
     {
-        
+        //Detect player in range
+        Collider[] hitPlayer = Physics.OverlapSphere(enemyAttackPoint.position, attackRange, playerLayer);
+
+        //Damage player
+        foreach (Collider player in hitPlayer)
+        {
+
+            player.GetComponent<PlayerAttackBehavior>().TakeDamage(attack);
+            Debug.Log("Attacked player");
+        }
+
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (enemyAttackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(enemyAttackPoint.position, attackRange);
+    }
+
+
 }
