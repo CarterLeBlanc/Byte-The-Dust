b119d1a0fdbc933c829ad274746bdd3bd68fbfc6
Carter LeBlanc <54289079+CarterLeBlanc@users.noreply.github.com>
2020-07-10 07:52:46 -0500

    Added comments.

--- Assets/Scripts/PlayerBaseBehavior.cs
+++ Assets/Scripts/PlayerBaseBehavior.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Holds the player's stats.
+/// </summary>
 public class PlayerBaseBehavior : MonoBehaviour
 {
-    //Default player stats
+    /// <summary>
+    /// Holds the player's max health.
+    /// </summary>
     public float playerMaxHealth = 100.0f;
+    /// <summary>
+    /// Holds the player's current health.
+    /// </summary>
     public float playerCurrentHealth = 100.0f;
+    /// <summary>
+    /// Holds the player's damage.
+    /// </summary>
     public float playerDamage = 5.0f;
-    public float playerSpeed = 3.0f;
+    /// <summary>
+    /// Holds the player's attack rate.
+    /// </summary>
     public float playerAttackRate = 2.0f;
 }
