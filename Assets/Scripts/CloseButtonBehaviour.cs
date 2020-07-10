173f5e109f68f184f550fcc7176d8f3a3147ced4
KyraWooders <wooderskyra@gmail.com>
2020-07-10 07:30:15 -0500

    Added Commits
    Added commits to code

--- Assets/Scripts/CloseButtonBehaviour.cs
+++ Assets/Scripts/CloseButtonBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CloseButtonBehaviour : MonoBehaviour
 {
+    /**
+     * Once the button is clicked, the scene will close.
+    **/
     public void CloseGame()
     {
         Application.Quit();
