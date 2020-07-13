039f1249015c570c5450c524ed2f325496190de8
PriceSarah <sfp1024@gmail.com>
2020-07-13 01:25:55 -0500

    deleted useless things

--- Assets/Scripts/PlayerMovementBehavior.cs
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 using UnityEngine.AI;
 
 /// <summary>
